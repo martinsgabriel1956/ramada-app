3104bd3b682131e257231816d3b215b2339dd11c
Gabriel Martins <martinsgabriel1956@gmail.com>
2024-11-19 01:33:05 -0300

    chore: set container

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "api/lib"
+import (
+	"api/lib"
+	"log"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+
 	server := lib.Server()
 	server.Run(":8080")
 }
